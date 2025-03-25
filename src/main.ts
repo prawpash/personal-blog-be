b5051088aa028c7525c5e74534b97635f1b5a14d
pasha <robbyprawira3@gmail.com>
2025-03-25 20:20:03 +0700

    feat(logger): Implement custom logger
    Implementing custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from '@infra/env/env.service';
+import { LoggerService } from '@infra/logging/LoggerService';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
 
+  app.useLogger(app.get(LoggerService));
   await app.listen(port);
 }
 
