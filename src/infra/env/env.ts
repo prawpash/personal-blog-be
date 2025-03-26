23de784b5d29da8d8a0427ac0481f3edb0037a2e
pasha <robbyprawira3@gmail.com>
2025-03-26 09:08:55 +0700

    feat(env): Add some properties
    Adding new properties related to the image upload process

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -8,6 +8,8 @@ export const envSchema = z.object({
   DATABASE_USER: z.string().default('root'),
   DATABASE_PASSWORD: z.string().default('root'),
   DATABASE_NAME: z.string().default('blog'),
+  UPLOAD_MAX_FILE_SIZE: z.coerce.number().default(1024 * 1024 * 1),
+  UPLOAD_FILE_TYPE: z.array(z.string()).default(['image/jpeg', 'image/png']),
 });
 
 export type Env = z.infer<typeof envSchema>;
