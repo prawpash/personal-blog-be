476200e2434ef4e837540c26529c326ef1d751e0
pasha <robby.pasha@salt.co.id>
2025-03-27 13:46:22 +0700

    refactor(typeorm): Separate the configuration
    Separating the typeorm configuration to another file, so it can be used to run
    migrations

--- src/infra/persistence/mysql/mysql.module.ts
+++ src/infra/persistence/mysql/mysql.module.ts
@@ -1,34 +1,16 @@
-import { EnvModule } from '@infra/env/env.module';
-import { EnvService } from '@infra/env/env.service';
+import { TypeORMDataSourceOptions } from '@infra/config/typeorm.config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostEntity } from './entities/PostEntity';
-import { ImageEntity } from './entities/ImageEntity';
-import { CategoryEntity } from './entities/CategoryEntity';
-import { UserEntity } from './entities/UserEntity';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      imports: [EnvModule],
-      useFactory: (envService: EnvService) => ({
-        type: 'mysql',
-        host: envService.get('DATABASE_HOST'),
-        port: envService.get('DATABASE_PORT'),
-        username: envService.get('DATABASE_USER'),
-        password: envService.get('DATABASE_PASSWORD'),
-        database: envService.get('DATABASE_NAME'),
-        entities: [PostEntity, ImageEntity, CategoryEntity, UserEntity],
-        synchronize: false,
-        migrations: [
-
-        ],
-        migrationsTableName: 'migrations',
+      useFactory: () => ({
+        ...TypeORMDataSourceOptions,
       }),
-      inject: [EnvService],
     }),
   ],
   providers: [],
   exports: [],
 })
-export class MysqlModule {}
+export class MysqlModule { }
