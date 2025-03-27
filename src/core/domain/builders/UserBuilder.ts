24c734de27a119b01df0badcb7eba01e880b6769
pasha <robby.pasha@salt.co.id>
2025-03-27 20:40:57 +0700

    feat(domain): Update User entity
    Adding capabilities to set createdAt and updatedAt properties

--- src/core/domain/builders/UserBuilder.ts
+++ src/core/domain/builders/UserBuilder.ts
@@ -9,6 +9,8 @@ export default class UserBuilder {
   private password: string;
   private profilePictureId: number | null;
   private postIds: number[];
+  private createdAt: Date;
+  private updatedAt: Date;
 
   constructor() {
     this.id = null;
@@ -18,6 +20,8 @@ export default class UserBuilder {
     this.password = '';
     this.profilePictureId = null;
     this.postIds = [];
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
   }
 
   build() {
@@ -39,34 +43,53 @@ export default class UserBuilder {
       this.password,
       this.profilePictureId,
       this.postIds,
+      this.createdAt,
+      this.updatedAt,
     );
   }
 
   setId(id: number) {
     this.id = id;
+    return this;
   }
 
   setName(name: string) {
     this.name = name;
+    return this;
   }
 
   setUsername(username: string) {
     this.username = username;
+    return this;
   }
 
   setEmail(email: string) {
     this.email = email;
+    return this;
   }
 
   setPassword(password: string) {
     this.password = password;
+    return this;
   }
 
   setProfilePictureId(profilePictureId: number) {
     this.profilePictureId = profilePictureId;
+    return this;
   }
 
   setPostIds(postIds: number[]) {
     this.postIds = postIds;
+    return this;
+  }
+
+  setCreatedAt(createdAt: Date) {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  setUpdatedAt(updatedAt: Date) {
+    this.updatedAt = updatedAt;
+    return this;
   }
 }
