65d1b69310f1a837958e72f2e963fae68e7b331a
pasha <robbyprawira3@gmail.com>
2025-03-28 20:11:58 +0700

    feat(domain): Add createdAt and updatedAt initialization to Image for timestamping
    Updated `Image` and `ImageBuilder` to support setting `createdAt` and
    `updatedAt`, enabling automatic timestamping during object creation.

--- src/core/domain/builders/ImageBuilder.ts
+++ src/core/domain/builders/ImageBuilder.ts
@@ -5,28 +5,51 @@ export default class ImageBuilder {
   private id: number | null;
   private fileName: string;
   private fileLocation: string;
+  private createdAt: Date;
+  private updatedAt: Date;
 
   constructor() {
     this.id = null;
     this.fileName = '';
     this.fileLocation = '';
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
   }
 
   build() {
     ImageValidator.validate(this.id, this.fileName, this.fileLocation);
 
-    return new Image(this.id, this.fileName, this.fileLocation);
+    return new Image(
+      this.id,
+      this.fileName,
+      this.fileLocation,
+      this.createdAt,
+      this.updatedAt,
+    );
   }
 
   setId(id: number) {
     this.id = id;
+    return this;
   }
 
   setFileName(fileName: string) {
     this.fileName = fileName;
+    return this;
   }
 
   setFileLocation(fileLocation: string) {
     this.fileLocation = fileLocation;
+    return this;
+  }
+
+  setCreatedAt(createdAt: Date) {
+    this.createdAt = createdAt;
+    return this;
+  }
+
+  setUpdatedAt(updatedAt: Date) {
+    this.updatedAt = updatedAt;
+    return this;
   }
 }
