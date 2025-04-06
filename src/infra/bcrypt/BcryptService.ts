33ecb3939b6cdeefe67a60fbd83d145a2f3ae251
pasha <robbyprawira3@gmail.com>
2025-04-07 05:55:49 +0700

    fix(bcrypt): cannot find the bcrypt instance
    Fix the code that cannot find the bcrypt instance

--- src/infra/bcrypt/BcryptService.ts
+++ src/infra/bcrypt/BcryptService.ts
@@ -1,20 +1,23 @@
 import { PasswordService } from '@core/application/services/PasswordService';
 import { EnvService } from '@infra/env/env.service';
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export default class BcryptService implements PasswordService {
   constructor(private readonly envService: EnvService) {}
 
   async hash(password: string): Promise<string> {
-    return await bcrypt.hash(
+    const hashedPassword = await bcrypt.hash(
       password,
       this.envService.get('PASSWORD_SALT_ROUNDS'),
     );
+
+    return hashedPassword;
   }
 
   async compare(plain: string, hashed: string): Promise<boolean> {
-    return await bcrypt.compare(plain, hashed);
+    const isMatch = await bcrypt.compare(plain, hashed);
+    return isMatch;
   }
 }
