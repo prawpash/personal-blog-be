3b888c54d9d28847a65ced772915fb5f4b4ae915
pasha <robbyprawira3@gmail.com>
2025-04-07 06:28:01 +0700

    feat(controller): implement the user controller
    Implement the user constroller

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,9 +1,11 @@
 import {
   Body,
+  ConflictException,
   Controller,
   Delete,
   Get,
   Inject,
+  NotFoundException,
   Param,
   Post,
   Put,
@@ -13,24 +15,75 @@ import { IdValidationDTO } from './dtos/IdValidationDTO';
 import { UpdateUserDTO } from './dtos/user/UpdateUserDTO';
 import { Logger } from '@core/application/services/Logger';
 import { InjectionToken } from '@infra/config/injectionToken.config';
+import { UserRepository } from '@core/domain/repositories/UserRepository';
+import GetUserByUsernameUseCase from '@core/application/usecases/user/GetUserByUsernameUseCase';
+import { NotFoundException as CoreNotFoundException } from '@core/exceptions/NotFoundException';
+import { ConflictException as CoreConflictException } from '@core/exceptions/ConflictException';
+import CreateUserUseCase from '@core/application/usecases/user/CreateUserUseCase';
+import { PasswordService } from '@core/application/services/PasswordService';
+import UserBuilder from '@core/domain/builders/UserBuilder';
 
 @Controller('users')
 export class UserController {
   constructor(
     @Inject(InjectionToken.LOGGER)
     private readonly logger: Logger,
-  ) {}
+    @Inject(InjectionToken.USER_REPOSITORY)
+    private readonly userRepository: UserRepository,
+    @Inject(InjectionToken.PASSWORD_SERVICE)
+    private readonly passwordService: PasswordService,
+  ) { }
 
   @Get(':username')
-  getUserByUsername(@Param('username') username: string) {
-    return `Get user by username: ${username}`;
+  async getUserByUsername(@Param('username') username: string) {
+    try {
+      const getUserByUsernameUseCase = new GetUserByUsernameUseCase(
+        this.userRepository,
+      );
+
+      const user = await getUserByUsernameUseCase.execute(username);
+
+      console.log(user);
+
+      return 'halo';
+
+      // return user;
+    } catch (error) {
+      if (error instanceof CoreNotFoundException) {
+        throw new NotFoundException(error.message);
+      }
+
+      throw error;
+    }
   }
 
   @Post()
-  createUser(@Body() createUserDTO: CreateUserDTO) {
-    this.logger.debug(createUserDTO);
+  async createUser(@Body() createUserDTO: CreateUserDTO) {
+    try {
+      const createUserUseCase = new CreateUserUseCase(
+        this.userRepository,
+        this.passwordService,
+      );
 
-    return 'Create user';
+      const userFromDTO = new UserBuilder()
+        .setName(createUserDTO.name)
+        .setUsername(createUserDTO.username)
+        .setEmail(createUserDTO.email)
+        .setPassword(createUserDTO.password)
+        .build();
+
+      const createdUser = await createUserUseCase.execute(userFromDTO);
+
+      this.logger.debug(createdUser);
+
+      return 'Create user';
+    } catch (error) {
+      if (error instanceof CoreConflictException) {
+        throw new ConflictException(error.message);
+      }
+
+      throw error;
+    }
   }
 
   @Put(':id')
--- src/presentation/controllers/dtos/user/CreateUserDTO.ts
+++ src/presentation/controllers/dtos/user/CreateUserDTO.ts
@@ -1,21 +1,33 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDTO {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(255)
   readonly name: string;
 
   @IsNotEmpty()
   @IsString()
+  @MaxLength(255)
   readonly username: string;
 
   @IsNotEmpty()
   @IsString()
   @IsEmail()
+  @MaxLength(255)
   readonly email: string;
 
   @IsNotEmpty()
   @IsString()
-  @IsEmail()
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters long',
+  })
+  @MaxLength(255)
   readonly password: string;
 }
