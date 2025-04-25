6e20fcaf0554836521be492ac2ae2275f04f063f
pasha <robbyprawira3@gmail.com>
2025-04-25 23:04:57 +0700

    docs(dto): Give an example value for id
    Give an example value for id in parameter

--- src/presentation/dtos/IdValidationDTO.ts
+++ src/presentation/dtos/IdValidationDTO.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumberString } from 'class-validator';
 
 export class IdValidationDTO {
+  @ApiProperty({
+    example: 1,
+  })
   @IsNotEmpty()
   @IsNumberString()
   @Transform(({ value }: { value: string }) => parseInt(value, 10))
